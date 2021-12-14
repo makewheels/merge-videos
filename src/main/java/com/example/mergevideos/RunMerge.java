018adfd5ebf07725f5c6b64a96b3a1b981c82317
miuser <miuser@email.com>
2021-12-14 17:46:00 +0800

    commit

--- src/main/java/com/example/mergevideos/RunMerge.java
+++ src/main/java/com/example/mergevideos/RunMerge.java
@@ -1,10 +1,20 @@
 package com.example.mergevideos;
 
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.probe.FFmpegFormat;
+import net.bramp.ffmpeg.probe.FFmpegProbeResult;
+
 import java.io.File;
+import java.io.IOException;
 
 public class RunMerge {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File originalFolder = new File("D:\\BaiduNetdiskDownload\\行车记录仪\\2021.03.06");
-
+        FFprobe ffprobe = new FFprobe("C:\\mysofts\\ffprobe.exe");
+        FFmpegProbeResult probeResult = ffprobe.probe(
+                "D:\\BaiduNetdiskDownload\\FILE201214-100956-000493F.MOV");
+        FFmpegFormat format = probeResult.getFormat();
+        double duration = format.duration;
+        System.out.println(duration);
     }
 }
