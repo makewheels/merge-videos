9729c55243c13d930a62c1bdb5bc26ee9cfc9fd5
miuser <miuser@email.com>
2021-12-14 17:37:36 +0800

    init

---
+++ src/main/java/com/example/mergevideos/MergeVideosApplication.java
@@ -0,0 +1,13 @@
+package com.example.mergevideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeVideosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MergeVideosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mergevideos/MergeVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mergevideos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeVideosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
