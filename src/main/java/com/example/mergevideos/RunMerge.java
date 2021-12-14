2da4ce2b330c764912127244d233722c8c27a510
miuser <miuser@email.com>
2021-12-14 17:44:29 +0800

    commit

---
+++ src/main/java/com/example/mergevideos/RunMerge.java
@@ -0,0 +1,10 @@
+package com.example.mergevideos;
+
+import java.io.File;
+
+public class RunMerge {
+    public static void main(String[] args) {
+        File originalFolder = new File("D:\\BaiduNetdiskDownload\\行车记录仪\\2021.03.06");
+
+    }
+}
